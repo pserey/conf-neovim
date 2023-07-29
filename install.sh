81b61d98e3ca3c6c6c6ebf20e031ab83d3ce5f2e
Pedro Serey <serey.pedro@gmail.com>
2023-07-29 11:00:08 -0300

    add: move nvim local files on install

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 cp -r nvim ~/.config/
 
 # install packer.nvim
+mv ~/.local/share/nvim/ ~/.local/share/backup-nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # sync plugins
